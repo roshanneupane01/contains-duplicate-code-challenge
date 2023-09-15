1409a9d5ddf7b2793fa3e7bf8637fd88ff9423a7
Roshan Neupane <roshanneupane01@gmail.com>
2023-09-14 21:58:37 -0500

    contains duplicate code challenge

---
+++ ContainsDuplicate.java
@@ -0,0 +1,22 @@
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+class ContainsDuplicate {
+
+    public static void main(String[] args) {
+        System.out.println(constainsDuplicate(new int[]{1,2,3,1}));
+    }
+
+    private static boolean constainsDuplicate(int[] nums) {
+        Set<Integer> set = new LinkedHashSet<>();
+        for(int element: nums) {
+            if(set.contains(element)){
+                return true;
+            }else{
+                set.add(element);
+            }
+        }
+        return false;
+    }
+}
